1c0f42e08ffddd981e63dd327e145f55f3322bff
yu <nagase.yuki0123@gmail.com>
2021-10-06 02:40:52 +0900

    first commit

---
+++ docker_simple_memo_php/common/header.php
@@ -0,0 +1,15 @@
+<?php 
+// @params $title
+// @return string
+  function getHeader($title) {
+    return <<<EOF
+    <head>
+        <meta charset="utf-8" />
+        <title>SimpleMemo | {$title}</title>
+        <link rel="stylesheet" type="text/css" href="../public/css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" href="../public/css/main.css" />
+        <script defer src="../public/js/all.js"></script>
+    </head>
+    EOF;
+  }
+?>
